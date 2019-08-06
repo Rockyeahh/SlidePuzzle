a11116b81005dc81975781d2d95fd3460b804f8e
Charlie <chaz.com@hotmail.co.uk>
2019-08-06 18:16:57 +0100

    Project begun. Block Control script barely started.

---
+++ Slide Puzzle/Assets/Scripts/BlockControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockControl : MonoBehaviour
+
+    // THIS NEEDS TO BE ATTACHED TO THE BLOCKS.
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        print("Mouse held down."); // Hold mouse down on the blocks.
+    }
+
+    // How do I check which block is being clicked on. Needs to print the name of the block each time it is printed on.
+
+}
