bfb4b26350e266be032038244d8cfbcc89f59c6b
Charlie <chaz.com@hotmail.co.uk>
2019-08-12 14:13:44 +0100

    Script now prints if clicking on block.

--- Slide Puzzle/Assets/Scripts/BlockControl.cs
+++ Slide Puzzle/Assets/Scripts/BlockControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BlockControl : MonoBehaviour
 
-    // THIS NEEDS TO BE ATTACHED TO THE BLOCKS.
+    // THIS NEEDS TO BE ATTACHED TO THE BLOCKS. Maybe not?
 {
     // Start is called before the first frame update
     void Start()
@@ -15,13 +15,26 @@ public class BlockControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+            //Debug.Log ("clicked");
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                //Debug.Log("Clicked");
+                Debug.Log(hit.collider.gameObject.name);
+                // hit.collider. some negative SFX says you can't move it.;
+            }
+        }
     }
 
-    private void OnMouseDown()
-    {
-        print("Mouse held down."); // Hold mouse down on the blocks.
-    }
+    //private void OnMouseDown()
+    //{
+    //    print("Mouse held down."); // Hold mouse down on the blocks.
+    //}
 
     // How do I check which block is being clicked on. Needs to print the name of the block each time it is printed on.
 
