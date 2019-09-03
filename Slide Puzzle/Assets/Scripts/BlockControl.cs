92c934589ab1b7431b99f9aa925370dcde8d4c8a
Charlie <chaz.com@hotmail.co.uk>
2019-09-03 16:37:33 +0100

    Basic Click and Drag movement added.

--- Slide Puzzle/Assets/Scripts/BlockControl.cs
+++ Slide Puzzle/Assets/Scripts/BlockControl.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class BlockControl : MonoBehaviour
 
-    // THIS NEEDS TO BE ATTACHED TO THE BLOCKS. Maybe not?
+// THIS NEEDS TO BE ATTACHED TO THE BLOCKS. Maybe not?
 {
 
     private GameObject selectedBlock; // should = highlighted or a different colour or something.
     private GameObject deselectedBlock;
     private bool dragging = false;
     private float distance;
-    private Vector2 startDist;
+    private Vector3 startDist;
 
     void OnMouseEnter()
     {
@@ -25,11 +25,12 @@ public class BlockControl : MonoBehaviour
 
     void OnMouseDown()
     {
+        print("Anything");
         distance = Vector3.Distance(transform.position, Camera.main.transform.position); // returns the distance between this scripts position and the cameras position.
         dragging = true;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // 
-        Vector2 rayPoint = ray.GetPoint(distance);
-        startDist = (Vector2)transform.position - rayPoint;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // It's a line from the camera to the input mouse position. In this case holding down mouse button.
+        Vector3 rayPoint = ray.GetPoint(distance); // Returns a Vector 3 point at the distance float along the ray. ?? How do I make it be a Vector 2?
+        startDist = (Vector3)transform.position - rayPoint; // It's a Vector2 transform.position - the ray point, which is the distance along the ray.
     }
 
     void OnMouseUp()
@@ -41,26 +42,29 @@ public class BlockControl : MonoBehaviour
     void Update()
     {
 
+        //if (Input.GetMouseButtonDown(0))
+        ////Debug.Log ("clicked");
+        //{
+        //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //    Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+        //    RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+        //    if (hit.collider != null)
+        //    {
+        //        //Debug.Log("Clicked");
+        //        Debug.Log(hit.collider.gameObject.name);
+        //        // hit.collider. some negative SFX says you can't move it.;
+        //    }
+        //}
+        //else { dragging = false; } // I think this may be stopping the dragging = true from working right.
+
         if (dragging)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector2 rayPoint = ray.GetPoint(distance);
+            Vector3 rayPoint = ray.GetPoint(distance);
             transform.position = rayPoint + startDist;
-        }
-
-        if (Input.GetMouseButtonDown(0))
-            //Debug.Log ("clicked");
-        {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if (hit.collider != null)
-            {
-                //Debug.Log("Clicked");
-                Debug.Log(hit.collider.gameObject.name);
-                // hit.collider. some negative SFX says you can't move it.;
-            }
+            print(transform.position);
+            print("The if dragging code has happend");
         }
     }
 
